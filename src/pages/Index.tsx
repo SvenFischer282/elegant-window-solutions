178cdac1261f6eb0780d6f1f87b36f301ef8c6ed
SvenFischer282 <81426840+SvenFischer282@users.noreply.github.com>
2025-12-13 17:13:34 +0100

    products updt

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -30,30 +30,35 @@ import carouselImg3 from "@/assets/nase_sluzby/carousel_imgs/3.jpg";
 
 const Index = () => {
   const products = [
-    { 
-      title: "Okná", 
+    {
+      title: "Okná",
       path: "/windows",
-      image: "https://vesnyanka.com.ua/wp-content/uploads/2022/10/valeriy-lobanov-sCFjj3evA1s-unsplash-1-scaled.jpg"
+      image:
+        "https://vesnyanka.com.ua/wp-content/uploads/2022/10/valeriy-lobanov-sCFjj3evA1s-unsplash-1-scaled.jpg",
     },
-    { 
-      title: "Dvere", 
+    {
+      title: "Dvere",
       path: "/doors",
-      image: "https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=800&q=80"
+      image:
+        "https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=800&q=80",
     },
     {
       title: "Posuvné systémy",
       path: "/sliding-systems",
-      image: "https://vesnyanka.com.ua/wp-content/uploads/2025/03/83d2470a-7357-4e2a-a0fc-3524bcc91433.webp"
+      image:
+        "https://vesnyanka.com.ua/wp-content/uploads/2025/03/83d2470a-7357-4e2a-a0fc-3524bcc91433.webp",
     },
-    { 
-      title: "Hliník", 
+    {
+      title: "Hliník",
       path: "/aluminum",
-      image: "https://vesnyanka.com.ua/wp-content/uploads/2025/03/DALL%C2%B7E-2025-03-31-21.43.31-A-modern-aluminum-and-glass-door-designed-for-contemporary-homes-and-commercial-spaces.-The-door-features-a-sleek-minimalistic-frame-with-a-large-gla.webp"
+      image:
+        "https://vesnyanka.com.ua/wp-content/uploads/2025/03/DALL%C2%B7E-2025-03-31-21.43.31-A-modern-aluminum-and-glass-door-designed-for-contemporary-homes-and-commercial-spaces.-The-door-features-a-sleek-minimalistic-frame-with-a-large-gla.webp",
     },
-    { 
-      title: "Fasády", 
+    {
+      title: "Fasády",
       path: "/facades",
-      image: "https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?w=800&q=80"
+      image:
+        "https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?w=800&q=80",
     },
   ];
 
@@ -107,7 +112,8 @@ const Index = () => {
         height="calc(100vh - 4rem)"
         staticText={{
           title: "Kvalitné okná a dvere",
-          subtitle: "Viac ako 20 rokov skúseností s výrobou a montážou okien, dverí a fasád"
+          subtitle:
+            "Viac ako 20 rokov skúseností s výrobou a montážou okien, dverí a fasád",
         }}
       />
 
@@ -139,7 +145,7 @@ const Index = () => {
               <Link
                 key={product.path}
                 to={product.path}
-                className="group relative aspect-[3/4] overflow-hidden rounded-lg w-[calc(50%-0.5rem)] md:w-[calc(33.333%-0.75rem)] lg:w-[calc(20%-0.8rem)]"
+                className="group relative aspect-[3/4] overflow-hidden  md:w-[calc(33.333%-0.75rem)] lg:w-[calc(20%-0.8rem)]"
               >
                 <img
                   src={product.image}
